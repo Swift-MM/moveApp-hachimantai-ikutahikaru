3c8132bd3bb504c6bed7ce75bab50a3a8423d096
ikutahikaru <hikaruikuta@hanae-no-MacBook-Pro.local>
2016-10-30 18:07:51 +0900

    add

---
+++ moveApp/NewtViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewtViewController.swift
+//  moveApp
+//
+//  Created by Hikaru Ikuta on 2016/10/30.
+//  Copyright © 2016年 Hikaru Ikuta. All rights reserved.
+//
+
+import UIKit
+
+class NewtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    }
---
+++ moveApp/NextNextViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NextNextViewController.swift
+//  moveApp
+//
+//  Created by Hikaru Ikuta on 2016/10/30.
+//  Copyright © 2016年 Hikaru Ikuta. All rights reserved.
+//
+
+import UIKit
+
+class NextNextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
